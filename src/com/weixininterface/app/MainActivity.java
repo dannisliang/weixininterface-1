531f496e71fc3b2bc47c3db2e2afbfd37cfd9f14
feiyangwei <cugb_feiyang@163.com>
2015-07-29 18:43:43 +0800

    添加空间

--- src/com/weixininterface/app/MainActivity.java
+++ src/com/weixininterface/app/MainActivity.java
@@ -1,36 +1,51 @@
 package com.weixininterface.app;
 
-import com.example.weixininterface.R;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.widget.Button;
+
+import com.example.weixininterface.R;
 
 public class MainActivity extends Activity {
-
+	private ViewPager mViewPager;
+	private FragmentPagerAdapter mAdapter;
+	
+	private Button weixin;
+	private Button tongxun;
+	private Button find;
+	private Button me;
+	
+	private int currentIndex;//ViewPager的当前选中页
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		weixin = (Button) findViewById(R.id.weixin);
+		tongxun = (Button) findViewById(R.id.tongxun);
+		find = (Button) findViewById(R.id.find);
+		me = (Button) findViewById(R.id.me);
+		
+		mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+		//注意这里
+		mAdapter = new FragmentPagerAdapter(null) {
+			
+			@Override
+			public int getCount() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public Fragment getItem(int arg0) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
